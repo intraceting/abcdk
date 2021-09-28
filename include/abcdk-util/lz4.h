8565804d47b0601f334f739acc3a754260ceb25a
zpcoding <intraceing@outlook.com>
2021-09-28 10:37:38 +0800

    update

---
+++ include/abcdk-util/lz4.h
@@ -0,0 +1,21 @@
+/*
+ * This file is part of ABCDK.
+ * 
+ * MIT License
+ * 
+ */
+#ifndef ABCDK_UTIL_LZ4_H
+#define ABCDK_UTIL_LZ4_H
+
+#include "abcdk-util/general.h"
+
+#ifdef  HAVE_LZ4
+#include <lz4.h>
+#endif //HAVE_LZ4
+
+__BEGIN_DECLS
+
+
+__END_DECLS
+
+#endif //ABCDK_UTIL_LZ4_H
