d28b69acd114fe0ed34e26a7cfa084738a816e5f
zpcoding <intraceing@outlook.com>
2022-08-19 17:17:11 +0800

    update

--- comm/easy.c
+++ comm/easy.c
@@ -500,7 +500,7 @@ void _abcdk_comm_easy_event_close(abcdk_comm_node_t *node)
     }
     else
     {
-        /*ACCEPT可能还未完成连接就已经断开了。*/
+        /*可能还未完成连接就已经断开了。*/
 
         abcdk_comm_get_sockaddr_str(node, sockname_str, peername_str);
         fprintf(stderr, "sockname(%s) -> peername(%s) disconnected.\n",sockname_str, peername_str);
