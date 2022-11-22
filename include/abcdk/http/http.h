dd51fa091a12cab9c77ac938425a34019fb26fee
zpcoding <intraceing@outlook.com>
2022-11-22 09:29:16 +0800

    update

---
+++ include/abcdk/http/http.h
@@ -0,0 +1,13 @@
+/*
+ * This file is part of ABCDK.
+ *
+ * MIT License
+ *
+ */
+#ifndef ABCDK_HTTP_HTTP_H
+#define ABCDK_HTTP_HTTP_H
+
+#include "abcdk/http/reply.h"
+#include "abcdk/http/request.h"
+
+#endif //ABCDK_HTTP_HTTP_H
