acbfe32ee3a76e5153d41707d7e5c02a324a24f5
zpcoding <intraceing@outlook.com>
2023-04-14 15:25:44 +0800

    update

---
+++ include/abcdk/util/enigma.h
@@ -0,0 +1,19 @@
+/*
+ * This file is part of ABCDK.
+ * 
+ * MIT License
+ * 
+ */
+#ifndef ABCDK_UTIL_ENIGMA_H
+#define ABCDK_UTIL_ENIGMA_H
+
+#include "abcdk/util/general.h"
+#include "abcdk/util/object.h"
+
+__BEGIN_DECLS
+
+//void abcdk_enigma(void *dst,const void *src,size_t size,uint8_t wheels[][255],int count);
+
+__END_DECLS
+
+#endif //ABCDK_UTIL_ENIGMA_H
---
+++ source/util/enigma.c
@@ -0,0 +1,8 @@
+/*
+ * This file is part of ABCDK.
+ *
+ * MIT License
+ *
+ */
+#include "abcdk/util/enigma.h"
+
