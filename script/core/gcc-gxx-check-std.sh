2819a108cfe6482f67d1a82b9052fe578935541b
zpcoding <intraceing@outlook.com>
2023-10-20 18:56:22 +0800

    update

---
+++ script/core/gcc-gxx-check-std.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This file is part of ABCDK.
+#  
+# MIT License
+##
+#
+SHELLDIR=$(cd `dirname $0`; pwd)
+
+
+#
+if [ $# -ne 2 ];then
+{
+    echo "22"
+    exit 22
+}
+fi
+
+#
+COMPILER=$1
+STD=$2
+
+#
+${COMPILER} -E -dM -std=${STD} - </dev/null >/dev/null 2>&1
+echo "$?"
+exit 0
