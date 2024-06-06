0490b0e426cb33ba9b38aaca6d2d1f1715a30d56
zpcoding <traceting@gmail.com>
2024-06-06 19:16:35 +0800

    update

--- lib/include/abcdk/ssl/easyssl.h
+++ lib/include/abcdk/ssl/easyssl.h
@@ -17,6 +17,20 @@ __BEGIN_DECLS
 typedef struct _abcdk_easyssl abcdk_easyssl_t;
 
 
+/**销毁。*/
+void abcdk_easyssl_destroy(abcdk_easyssl_t **ctx);
+
+/**创建。*/
+abcdk_easyssl_t *abcdk_easyssl_create(const uint8_t *dict,size_t rotor);
+
+/**
+ * 关联句柄。
+ * 
+ * @return 旧的句柄。
+*/
+int abcdk_easyssl_set_fd(abcdk_easyssl_t *ctx,int fd);
+
+
 __END_DECLS
 
 #endif //ABCDK_SSL_EASYSSL_H
