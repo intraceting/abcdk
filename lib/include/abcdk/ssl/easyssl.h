2702f8634cf7920fe13d39b44836ba8d604f21a9
zpcoding <traceting@gmail.com>
2024-06-06 18:34:36 +0800

    update

---
+++ lib/include/abcdk/ssl/easyssl.h
@@ -0,0 +1,24 @@
+/*
+ * This file is part of ABCDK.
+ * 
+ * MIT License
+ * 
+ */
+#ifndef ABCDK_SSL_EASYSSL_H
+#define ABCDK_SSL_EASYSSL_H
+
+#include "abcdk/util/general.h"
+#include "abcdk/util/enigma.h"
+#include "abcdk/util/object.h"
+
+__BEGIN_DECLS
+
+/** 简单的SSL通讯。 */
+typedef struct _abcdk_easyssl abcdk_easyssl_t;
+
+
+__END_DECLS
+
+#endif //ABCDK_SSL_EASYSSL_H
+
+
