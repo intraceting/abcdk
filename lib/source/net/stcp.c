2907fdb79ad3663b92e9a869e0a7a30256b6a27a
zpcoding <traceting@gmail.com>
2024-12-13 23:06:26 +0800

    update

--- lib/source/net/stcp.c
+++ lib/source/net/stcp.c
@@ -785,8 +785,12 @@ void _abcdk_stcp_handshake(abcdk_stcp_node_t *node)
         }
         else
         {
+#if 0
             if(node->cfg.ssl_scheme == ABCDK_STCP_SSL_SCHEME_PKIS)
             {
+                /*不清楚改的什么，导致SSL_get_error无法获取正确的返回值，暂时用下面的代码替换一下。*/
+                //ssl_err = SSL_get_error(node->openssl_ssl, ssl_chk);
+
                 BIO *bio_ctx = SSL_get_rbio(node->openssl_ssl);
 
                 if(BIO_should_read(bio_ctx) && BIO_should_retry(bio_ctx))
@@ -797,6 +801,7 @@ void _abcdk_stcp_handshake(abcdk_stcp_node_t *node)
                     ssl_err = SSL_ERROR_SSL;
             }
             else
+#endif
             {
                 /*必须通过返回值获取出错码。*/
                 ssl_err = SSL_get_error(node->openssl_ssl, ssl_chk);
