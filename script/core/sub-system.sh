e740dca25f6057e2661266402bbcbcd5c8073f20
intraceting <traceting@gmail.com>
2025-01-07 18:14:24 +0800

    update

--- script/core/sub-system.sh
+++ script/core/sub-system.sh
@@ -5,9 +5,9 @@
 # Copyright (c) 2021 The ABCDK project authors. All Rights Reserved.
 #
 
+#
+SHELLDIR=$(cd `dirname $0`; pwd)
 
-
-# 以下代码来自于网友，启动基本的子系统，但未配置DNS解析。
 # 
 # 1：子系纺启动后，可以通过下面的方法安装或更新软件包。
 # #apt update
@@ -19,38 +19,50 @@
 #
 
 
-mnt() {
-	echo "MOUNTING"
-	sudo mount -t proc /proc ${2}proc
-	sudo mount -t sysfs /sys ${2}sys
-	sudo mount -o bind /dev ${2}dev
-	sudo mount -o bind /dev/pts ${2}dev/pts
-	sudo chroot ${2}
-}
-umnt() {
-	echo "UNMOUNTING"
-	sudo umount ${2}proc
-	sudo umount ${2}sys
-	sudo umount ${2}dev/pts
-	sudo umount ${2}dev
+#挂载.
+mnt() 
+{
+	echo "正在挂载……"
+	mount -t proc /proc ${1}/proc
+	mount -t sysfs /sys ${1}/sys
+	mount -o bind /dev ${1}/dev
+	mount -o bind /dev/pts ${1}/dev/pts
+	chroot ${1}
+	echo "挂载完成。"
 }
 
-if [ "$1" == "-m" ] && [ -n "$2" ] ;
-then
-	mnt $1 $2
-elif [ "$1" == "-u" ] && [ -n "$2" ];
-then
-	umnt $1 $2
+#卸载.
+umnt() 
+{
+	echo "正在卸载……"
+	umount ${1}/proc
+	umount ${1}/sys
+	umount ${1}/dev/pts
+	umount ${1}/dev
+	echo "卸载完成。"
+}
+
+if [ "$(id -u)" -ne 0 ]; then
+{
+	echo "权限不足，仅允许root用户执行操作。"
+	exit 1
+}
+elif [ "$1" == "-m" ] && [ -n "$2" ]; then
+{
+	$(mnt "$2")
+	exit $?
+}
+elif [ "$1" == "-u" ] && [ -n "$2" ]; then
+{
+	$(umnt "$2")
+	exit $?
+}
 else
-	echo ""
-	echo "Either 1'st, 2'nd or both parameters were missing"
-	echo ""
-	echo "1'st parameter can be one of these: -m(mount) OR -u(umount)"
-	echo "2'nd parameter is the full path of rootfs directory(with trailing '/')"
-	echo ""
-	echo "For example: ch-mount -m /media/sdcard/"
-	echo ""
-	echo 1st parameter : ${1}
-	echo 2nd parameter : ${2}
+{
+	echo "至少需要两个参数。"
+	echo "第一个参数是操作码。支持挂载(-m)或卸载(-u)两个操作码。"
+	echo "第二个参数是rootfs路径。必需是完整路径，否则可能发生意料之外的错误。"
+	exit 22
+}
 fi
 
