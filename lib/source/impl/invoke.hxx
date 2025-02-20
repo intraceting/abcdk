3bc59663de7d2fe188d083c6b75eaea01a1dd248
intraceting <traceting@gmail.com>
2025-02-20 12:11:33 +0800

    update

---
+++ lib/source/impl/invoke.hxx
@@ -0,0 +1,22 @@
+/*
+ * This file is part of ABCDK.
+ *
+ * Copyright (c) 2025 The ABCDK project authors. All Rights Reserved.
+ *
+ */
+#ifndef ABCDK_IMPL_INVOKE_HXX
+#define ABCDK_IMPL_INVOKE_HXX
+
+#include "abcdk/util/defs.h"
+
+#ifdef __NVCC__
+#define ABCDK_INVOKE_DEVICE  __device__ __forceinline__
+#define ABCDK_INVOKE_HOST __host__ __forceinline__
+#define ABCDK_INVOKE_GLOBAL __global__
+#else //__NVCC__
+#define ABCDK_INVOKE_DEVICE __attribute__((always_inline))
+#define ABCDK_INVOKE_HOST __attribute__((always_inline))
+#define ABCDK_INVOKE_GLOBAL __attribute__((always_inline))
+#endif //__NVCC__
+
+#endif //ABCDK_IMPL_INVOKE_HXX
