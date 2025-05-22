ab0911b088aa72694bbed4f484b3e88d62d7a28b
intraceting <traceting@gmail.com>
2025-05-22 21:18:50 +0800

    update

--- makefile.pack.mk
+++ makefile.pack.mk
@@ -72,7 +72,7 @@ pack-rt-deb: pack-rt-prepare
 		-d FILES_NAME=${PACK_TMP}/${RT_PACKAGE_NAME}.filelist.txt \
 		-d POST_NAME=${PACK_TMP}/${RT_PACKAGE_NAME}.post.sh \
 		-d POSTUN_NAME=${PACK_TMP}/${RT_PACKAGE_NAME}.postun.sh \
-		-d REQUIRE_LIST="glibc"
+		-d REQUIRE_LIST="libc-bin"
 #复制到临时的系统根路径。
 	cp -rf ${PACK_TMP}/${RT_PACKAGE_NAME}.deb.ctl ${RT_SYSROOT_PREFIX}/DEBIAN
 #创建软链接，因为dpkg-shlibdeps要使用debian/control文件。下同。
