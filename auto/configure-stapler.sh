81033070fb8163c37de619b33819360f168dbad3
intraceting <traceting@gmail.com>
2025-07-09 15:34:46 +0800

    update

--- auto/configure-stapler.sh
+++ auto/configure-stapler.sh
@@ -20,10 +20,12 @@ checkReturnCode()
 #加载配置环境。
 source ${1}
 
+#   -d PACKAGE_SUFFIX="-stapler-gcc_${STAPLER_TARGET_COMPILER_VERSION}-glibc_${STAPLER_TARGET_GLIBC_MAX_VERSION}" \
+
 #
 ${SHELLDIR}/../configure.sh \
     -d INSTALL_PREFIX="${STAPLER_RELEASE_PATH}" \
-    -d PACKAGE_SUFFIX="-stapler-gcc_${STAPLER_TARGET_COMPILER_VERSION}-glibc_${STAPLER_TARGET_GLIBC_MAX_VERSION}" \
+    -d PACKAGE_SUFFIX="-stapler-glibc_${STAPLER_TARGET_GLIBC_MAX_VERSION}" \
     -d THIRDPARTY_PACKAGES="openssl,ffmpeg,opencv,live555,qrencode,curl,sqlite,nghttp2,eigen" \
     -d COMPILER_PREFIX=${STAPLER_TARGET_COMPILER_PREFIX}
 checkReturnCode
