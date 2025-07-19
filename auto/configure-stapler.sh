2f0548ce9a9d078e945a3c5c55c76f607a360e9f
intraceting <traceting@gmail.com>
2025-07-19 13:42:05 +0800

    update

--- auto/configure-stapler.sh
+++ auto/configure-stapler.sh
@@ -27,6 +27,5 @@ ${SHELLDIR}/../configure.sh \
     -d INSTALL_PREFIX="${STAPLER_RELEASE_PATH}" \
     -d PACKAGE_SUFFIX="-stapler-glibc_${STAPLER_TARGET_GLIBC_MAX_VERSION}" \
     -d THIRDPARTY_PACKAGES="openssl,ffmpeg,opencv,live555,qrencode,curl,sqlite,nghttp2,eigen" \
-    -d COMPILER_LD_FLAGS="-Wl,-rpath=${STAPLER_RELEASE_PATH}/lib/compat/ -Wl,-rpath=${STAPLER_RELEASE_PATH}/lib64/compat/" \
     -d COMPILER_PREFIX=${STAPLER_TARGET_COMPILER_PREFIX}
 checkReturnCode
