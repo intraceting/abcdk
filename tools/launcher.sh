fe00ec1059a1ebf7fd92d1a3b2aee86ba88639c9
intraceting <traceting@gmail.com>
2025-07-26 15:20:03 +0800

    update

---
+++ tools/launcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# This file is part of ABCDK.
+#  
+# Copyright (c) 2025 The ABCDK project authors. All Rights Reserved.
+# 
+##
+#
+SHELLNAME=$(basename ${0})
+SHELLDIR=$(cd `dirname ${0}`; pwd)
+
+#导出必要的环境变量。
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SHELLDIR}:${SHELLDIR}/lib:${SHELLDIR}/lib/compat:${SHELLDIR}/lib64:${SHELLDIR}/lib64/compat
+
+#启动可执行程序。
+${0}.exe $@
+exit $?
