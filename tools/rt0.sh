b387b57527f0c8807fd6ca8267b1ccedbe28ba9b
intraceting <traceting@gmail.com>
2025-07-26 17:11:51 +0800

    update

--- tools/rt0.sh
+++ tools/rt0.sh
@@ -10,7 +10,7 @@ SHELLNAME=$(basename ${0})
 SHELLDIR=$(cd `dirname ${0}`; pwd)
 
 #导出必要的环境变量。
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SHELLDIR}:${SHELLDIR}/lib:${SHELLDIR}/lib/compat:${SHELLDIR}/lib64:${SHELLDIR}/lib64/compat
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SHELLDIR}:${SHELLDIR}/../lib64:${SHELLDIR}/../lib64/compat:${SHELLDIR}/../lib:${SHELLDIR}/../lib/compat
 
 #启动可执行程序。
 ${0}.exe $@
